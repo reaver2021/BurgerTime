38b2846717b86894e9835b464ae82be1ebcaa70e
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-09-21 09:34:25 -0700

    fixed an orm part in the models

--- models/burger.js
+++ models/burger.js
@@ -8,7 +8,7 @@ var burger = {
     },
 
     insertOne: function (cols, vals, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
+        orm.insertOne("burgers", cols, vals, function (res) {
             cb(res);
         });
     },

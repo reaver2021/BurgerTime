1ff5b6e07222bfde4a8d8b796dd2d71248eb58e1
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-09-21 19:52:18 -0700

    changed small things

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 // Create the router for the app, and export the router at the end of your file.
-var router = express.router();
+var router = express.Router();
 // Create routes and set up logic where required.
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
